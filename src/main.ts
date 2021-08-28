bd1a6969192d92674ea75f41279f339def9641f1
rturaj <roman.turaj@auroria.pl>
2021-08-28 19:53:53 +0200

    basic PWA support

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import store from './store'
+import "./registerServiceWorker";
 
 createApp(App).use(store).mount('#app')
