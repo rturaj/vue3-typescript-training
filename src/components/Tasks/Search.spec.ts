06f1cfba8b548a7ef25709862d438a9d177cad29
rturaj <roman.turaj@auroria.pl>
2021-08-28 14:22:16 +0200

    test: Tasks/Search

---
+++ src/components/Tasks/Search.spec.ts
@@ -0,0 +1,21 @@
+import { mount } from '@vue/test-utils'
+import Search from './Search.vue'
+import faker from 'faker';
+
+describe('Employees/Search.vue', () => {
+  const wrapper = mount(Search);
+
+  test('renders', async () => {
+    expect(wrapper.findAllComponents({ name: 'AppInput' })).toHaveLength(1);
+    expect(wrapper.find('[label="Name"]')).not.toBe(null);
+  })
+
+  test('emits search event', async () => {
+    const name = faker.git.commitMessage()
+    await wrapper.find('#Name').setValue(name);
+    expect(wrapper.emitted('search')![0]).toEqual([{ name }])
+  })
+
+})
+
+
